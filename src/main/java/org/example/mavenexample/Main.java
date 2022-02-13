1ff243cd8c633ae5c86898daf5aca7c5e29c234c
nastya <anastasiasu4ulo@gmail.com>
2022-02-13 14:29:48 +0200

    Copy to file ver 1

--- src/main/java/org/example/mavenexample/Main.java
+++ src/main/java/org/example/mavenexample/Main.java
@@ -1,8 +1,23 @@
 package org.example.mavenexample;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+        File file = new File("E:/read.txt");
+        File fileToWrite = new File("E:/write.txt");
+
+        InputStream in = new FileInputStream(file);
+        OutputStream out = new FileOutputStream(fileToWrite);
+
+        byte[] bytes = in.readAllBytes();
+
+        for (int i = 0; i < bytes.length; i++){
+            out.write(bytes[i]);
+        }
+
+        System.out.println("You can try to open file");
     }
+
 }
